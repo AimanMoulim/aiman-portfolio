773894cf62ed31af2a7ecaea482232b9ec6f6a74
moulimaiman <aiman.moulimai@gmail.com>
2026-07-07 16:48:46 +0100

    Fix Error Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "export",
   images: {
     unoptimized: true,
   },
-}
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+};
 
-export default nextConfig
+export default nextConfig;
