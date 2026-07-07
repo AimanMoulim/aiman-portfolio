6464fbbec2575a2eae0a71e4bb5a00ba5a892277
moulimaiman <aiman.moulimai@gmail.com>
2026-07-07 18:54:01 +0100

    Fix Vercel deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
-}
+};
 
-export default nextConfig
+export default nextConfig;
